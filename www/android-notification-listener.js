9217ea6e9be7f8abe7e08396bcabdb13c4aff04e
dead pool <deadpool@gmail.com>
2019-04-03 18:58:22 +0700

    init repo

--- www/android-notification-listener.js
+++ www/android-notification-listener.js
@@ -3,8 +3,8 @@
 module.exports = {
   // value must be an ArrayBuffer
   listen: function(success, failure) {
-    console.log("初始化 AndroidNotificationListener");
+    console.log("Initializing AndroidNotificationListener cordova plugin...");
     
-    cordova.exec(success, failure, "AndroidNotificationListenerPluginEntry", "listen", []);
+    cordova.exec(success, failure, "AndroidNotificationListenerPluginEntry", "listen", ['OK']);
   }
 };
