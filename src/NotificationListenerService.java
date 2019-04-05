3c8456cf1923ff1529d23343c13870d42b1790c7
dead pool <deadpool@gmail.com>
2019-04-05 21:58:40 +0700

    修改文件名称

--- src/NotificationListenerService.java
+++ src/NotificationListenerService.java
@@ -31,13 +31,13 @@ import java.util.List;
  * Android
  * 消息监听服务参见：//http://developer.android.com/reference/android/service/notification/NotificationListenerService.html
  */
-public class NotificationListenerService2 extends NotificationListenerService {
+public class NotificationListenerService extends NotificationListenerService {
   private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
 
   private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
   private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
 
-  private static final String TAG = NotificationListenerService2.class.getSimpleName();
+  private static final String TAG = NotificationListenerService.class.getSimpleName();
 
   private static final String PKG_WHITE_LIST = "com.google.android.googlequicksearchbox";
 
@@ -178,7 +178,7 @@ public class NotificationListenerService2 extends NotificationListenerService {
    */
   static void toggleNotificationListenerService(Context context) {
     PackageManager pm = context.getPackageManager();
-    ComponentName thisComponent = new ComponentName(context, NotificationListenerService2.class);
+    ComponentName thisComponent = new ComponentName(context, NotificationListenerService.class);
 
     pm.setComponentEnabledSetting(thisComponent, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
         PackageManager.DONT_KILL_APP);
